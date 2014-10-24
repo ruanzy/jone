99ab8f3966f3d74ffbfa2cab32c85d67ef5d7386
ruanzy <ruanzhiyong6496@163.com>
2014-10-24 15:18:38 +0800

    optimize

--- src/org/rzy/web/Context.java
+++ src/org/rzy/web/Context.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class Context
+class Context
 {
 	private ServletContext servletContext;
 
