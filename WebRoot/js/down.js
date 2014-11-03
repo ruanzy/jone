5ce39e570a0ebfb511c93a357eb21b9b6d4abe62
ruanzy <ruanzhiyong6496@163.com>
2014-11-03 11:27:03 +0800

    optimize

--- WebRoot/js/down.js
+++ WebRoot/js/down.js
@@ -10,8 +10,8 @@
 		}
 	};
 	$.fn.SideDown.defaults = {
-		width : 120,
-		trigger : 'click'
+		width : 100,
+		trigger : 'hover'
 	};
 	$.fn.SideDown.methods = {
 		init : function(options) {
@@ -19,67 +19,61 @@
 			return this.each(function() {
 				var me = $(this);
 				var dt = $('dt', this);
-				var dd = $('dd', this);
-				var rdm = new Date().getTime() + '_' + Math.floor(Math.random()*(100 - 1 + 1) + 1);
+				var dd = $('dd', this).width(opts.width).hide();
+				var rdm = new Date().getTime() + '_'
+						+ Math.floor(Math.random() * (100 - 1 + 1) + 1);
 				dt.attr('id', 'dt_' + rdm);
 				var items = opts.items;
-				if (items){
+				var url = opts.url;
+				if (items) {
 					var html = [];
-					for(var k in items){
-						if(items[k] === '-'){
-							html.push("<div class='divider'></div>");
-						}else{
-							var icon = items[k].icon||'';
-							var text = items[k].text||'';
-							var url = items[k].url;
-							var action = items[k].action;
-							var str = "<div class='item'><a";
-							if(url&&!action){
-								str += " href='javascript:" + url + "'";
-							}
-							str += "><i class='" + icon + "'></i><span style='padding-left:8px;'>" + text + "</span></a></div>";
+					for ( var k in items) {
+						if (items[k] === '-') {
+							html.push("<div class='item' idx='" + k
+									+ "'><div class='divider'></div></div>");
+						} else {
+							var icon = items[k].icon || '';
+							var text = items[k].text || '';
+							var str = "<div class='item' idx='" + k
+									+ "'><a href='javascript:'><i class='"
+									+ icon
+									+ "'></i><span style='padding-left:8px;'>"
+									+ text + "</span></a></div>";
 							html.push(str);
 						}
 					}
 					dd.html(html.join(''));
-					
-					for(var k in items){
-						if(items[k] === '-'){
-							continue;
-						}else{
-							var action = items[k].action;
-							if(action){
-								var div = $('div:nth(' + k + ')', dd);
-								div.click(function(e){
-									e.preventDefault();
-									e.stopPropagation();
-									action();
-								});
-							}
-							var str = "<div class='item'><a";
-							if(url&&!action){
-								str += " href='javascript:" + url + "'";
-							}
-							str += "><i class='" + icon + "'></i><span style='padding-left:8px;'>" + text + "</span></a></div>";
-							html.push(str);
+					$('div.item', dd).click(function(e) {
+						var idx = $(this).attr('idx');
+						var action = items[idx].action;
+						if (action) {
+							action.call();
 						}
-					}
-				}	
-				dt.click(function(e){
-					//var t = e.target;
-					//if(t.tagName == 'DT'){
+						dd.hide();
+					});
+				} else if (url) {
+					dd.load(url);
+				}
+				if (opts.trigger == 'click') {
+					me.click(function(e) {
 						dd.show();
-					//}
-						e.stopPropagation();
-				});
-				$(document).bind("click",function(e){ 
-					var target = $(e.target); 
-					if(target.closest('#dt_' + rdm).length == 0){ 
-						dd.hide(); 
-					} else{
-						dd.show(); 
-					}
-				});
+					});
+					$(document).bind("click", function(e) {
+						var target = $(e.target);
+						if (target.closest('#dt_' + rdm).length == 0) {
+							dd.hide();
+						} else {
+							dd.show();
+						}
+					});
+				}
+				if (opts.trigger == 'hover') {
+					me.hover(function(e) {
+						dd.show();
+					}, function(e) {
+						dd.hide();
+					});
+				}
 			});
 		}
 	};
--- WebRoot/js/index.js
+++ WebRoot/js/index.js
@@ -9,32 +9,6 @@ $(function() {
 	 * $('#center').Tabs(); $('#center').Tabs('add', { title : '欢迎使用', url :
 	 * 'welcome.jsp' });
 	 **************************************************************************/
-	/**$('#info').bind(
-			'click',
-			function(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				var pos = [ $(this).offset().left + $(this).outerWidth() - 122,
-						$(this).offset().top + $(this).outerHeight() + 1 ];
-				$.dropdown({
-					pos : pos,
-					items : [ {
-						icon : 'icon-cog',
-						text : '设置'
-					}, {
-						icon : 'icon-user',
-						text : '个人资料'
-					}, '-', {
-						icon : 'icon-off',
-						text : '退出',
-						url : 'common/logout'
-					} ]
-				});
-
-				$(document).click(function() {
-					$.dropdown.close();
-				});
-			});**/
 	var items = [ {
 		icon : 'icon-cog',
 		text : '设置'
@@ -59,34 +33,6 @@ $(function() {
 	function logout(){
 		document.location = 'common/logout';
 	}
-	var content = [];
-	content.push('<ul class=r-dropdown>');
-	for(var k in items){
-		if(items[k] === '-'){
-			content.push("<li class='divider'></li>");
-		}else{
-			var icon = items[k].icon||'';
-			var text = items[k].text||'';
-			var url = items[k].url;
-			var action = items[k].action;
-			var tag = "<li><a";
-			if(url&&!action){
-				tag += " href='" + url + "'";
-			}
-			tag += "><i class='" + icon + "'></i><span style='padding-left:8px;'>" + text + "</span></a></li>";
-			content.push(tag);
-			/**var row = $(tag).appendTo(ul);	
-			if(action){									
-				row.find('a').click(function(e){
-					e.preventDefault();
-					e.stopPropagation();
-					action();
-					$.dropdown.close();
-				});
-			}**/				
-		}
-	}
-	content.push('</ul>');
 	$('#asd').SideDown({
 		items:items
 	});
