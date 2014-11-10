54d888d9ae6d9fcf57f546cbb45f3c4315368ad7
ruanzy <ruanzhiyong6496@163.com>
2014-11-10 14:20:36 +0800

    optimize

--- src/org/rzy/util/IOUtil.java
+++ src/org/rzy/util/IOUtil.java
@@ -8,52 +8,31 @@ public class IOUtil
 {
 	public static String tail(String file, long lines)
 	{
-		// 定义结果集
-		StringBuffer result = new StringBuffer();
-		// 行数统计
+		String result = null;
 		long count = 0;
-		
 		File f = new File(file);
-
-		// 排除不可读状态
 		if (!f.exists() || f.isDirectory() || !f.canRead())
 		{
 			return null;
 		}
-
-		// 使用随机读取
-		RandomAccessFile fileRead = null;
+		RandomAccessFile rf = null;
 		try
 		{
-			// 使用读模式
-			fileRead = new RandomAccessFile(f, "r");
-			// 读取文件长度
-			long length = fileRead.length();
-			// 如果是0，代表是空文件，直接返回空结果
+			rf = new RandomAccessFile(f, "r");
+			long length = rf.length();
 			if (length == 0L)
 			{
 				return "";
 			}
 			else
 			{
-				// 初始化游标
 				long pos = length - 1;
 				while (pos > 0)
 				{
 					pos--;
-					// 开始读取
-					fileRead.seek(pos);
-					// 如果读取到\n代表是读取到一行
-					if (fileRead.readByte() == '\n')
+					rf.seek(pos);
+					if (rf.readByte() == '\n')
 					{
-						// 使用readLine获取当前行
-						String line = fileRead.readLine();
-						// 保存结果
-						result.append(line);
-						
-						result.append("\r\n");
-
-						// 行数统计，如果到达了numRead指定的行数，就跳出循环
 						count++;
 						if (count == lines)
 						{
@@ -63,9 +42,11 @@ public class IOUtil
 				}
 				if (pos == 0)
 				{
-					fileRead.seek(0);
-					result.append(fileRead.readLine());
+					rf.seek(0);
 				}
+				byte[] bytes = new byte[(int) (length - pos)];
+				rf.read(bytes);
+				result = new String(bytes);
 			}
 		}
 		catch (IOException e)
@@ -74,11 +55,11 @@ public class IOUtil
 		}
 		finally
 		{
-			if (fileRead != null)
+			if (rf != null)
 			{
 				try
 				{
-					fileRead.close();
+					rf.close();
 				}
 				catch (Exception e)
 				{
