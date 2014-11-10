26ccf5f76aef1c9b03946993419c06f721337522
ruanzy <ruanzhiyong6496@163.com>
2014-11-10 10:21:26 +0800

    optimize 日志记录的问题

--- src/log/FileLogHandler.java
+++ src/log/FileLogHandler.java
@@ -1,5 +1,6 @@
 package log;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.apache.commons.io.IOUtils;
@@ -9,9 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
 
-public class DefaultLogHandler implements LogHandler
+public class FileLogHandler implements LogHandler
 {
-	Logger logger = LoggerFactory.getLogger(DefaultLogHandler.class);
+	Logger logger = LoggerFactory.getLogger(FileLogHandler.class);
 
 	public void handler(Log log)
 	{
@@ -30,8 +31,7 @@ public class DefaultLogHandler implements LogHandler
 		logs.append(sid).append("|");
 		logs.append(1).append("|");
 		logs.append(requestBody);
-		logger.debug(logs.toString());
-		String logfile = System.getProperty("logDir") + "log.txt";
+		String logfile = System.getProperty("logDir") + File.separator + "log.txt";
 		FileWriter fw = null;
 		try
 		{
