0ca6c31727f3008a97c2536e20ac23e42e10d476
ruanzy <ruanzhiyong6496@163.com>
2015-01-28 09:32:42 +0800

    session user

--- src/org/rzy/web/WebUtil.java
+++ src/org/rzy/web/WebUtil.java
@@ -308,8 +308,8 @@ public class WebUtil
 	}
 
 	public static void setUserinfo(String userinfo)
-	{
-		WebUtil.Session.attr("RZY_USER", userinfo);
+	{	
+		WebUtil.Session.attr("RZY_USER", userinfo.split("_")[0]);
 		// String domain = WebUtil.Request.get().getServerName();
 		Cookie token = new Cookie("SSOTOKEN", userinfo);
 		token.setMaxAge(30 * 60);
