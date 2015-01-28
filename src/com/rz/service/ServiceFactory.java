a51e026a7f03ac501c9594742665f43948eb868f
ruanzy <ruanzhiyong6496@163.com>
2015-01-28 10:44:51 +0800

    optimize

--- src/com/rz/service/ServiceFactory.java
+++ src/com/rz/service/ServiceFactory.java
@@ -1,9 +1,13 @@
 package com.rz.service;
 
+import com.rz.util.WebUtil;
+
 public class ServiceFactory
 {
 	public static Service create()
 	{
-		return new LocalService();
+		Service service = new LocalService();
+		service.setUser(WebUtil.getUser());
+		return service;
 	}
 }
