e711932139ba63a70748930896da092fedca553e
ruanzy <ruanzhiyong6496@163.com>
2015-03-09 10:05:27 +0800

    logs

--- src/action/MyInitializer.java
+++ src/action/MyInitializer.java
@@ -9,6 +9,8 @@ public class MyInitializer implements Initializer
 
 	public void init(ServletContext context)
 	{
+		System.setProperty("logs", "D:/jone/logs");
+		
 		context.setAttribute("allres", new PmsService().res());
 	}
 
