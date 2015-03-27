8ae2f39818c5f08b455079e83f6b6935aa7e71f4
ruanzy <ruanzhiyong6496@163.com>
2015-03-27 11:22:30 +0800

    Uploader

--- src/com/rz/util/Uploader.java
+++ src/com/rz/util/Uploader.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -30,36 +31,52 @@ public class Uploader
 		items = new ArrayList<FileItem>();
 	}
 
-	public static Uploader prepare()
+	public static Uploader prepare(long maxSize) throws Exception
 	{
-		Uploader uploader = new Uploader();
-		try
+		boolean isMultipart = ServletFileUpload.isMultipartContent(WebUtil.Request.get());
+		if (!isMultipart)
 		{
-			String temp = System.getProperty("java.io.tmpdir");
-			DiskFileItemFactory factory = new DiskFileItemFactory();
-			factory.setRepository(new File(temp));
-			factory.setSizeThreshold(1024 * 1024 * 10);
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			upload.setHeaderEncoding("utf-8");
-			List<FileItem> list = upload.parseRequest(WebUtil.Request.get());
-			for (FileItem item : list)
+			return null;
+		}
+		Uploader uploader = new Uploader();
+		String temp = System.getProperty("java.io.tmpdir");
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setRepository(new File(temp));
+		factory.setSizeThreshold(1024 * 1024 * 10);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setHeaderEncoding("utf-8");
+		if (maxSize > 0)
+		{
+			upload.setSizeMax(maxSize);
+		}
+		List<FileItem> list = upload.parseRequest(WebUtil.Request.get());
+		for (FileItem item : list)
+		{
+			if (item.isFormField())
 			{
-				if (item.isFormField())
-				{
-					String name = item.getFieldName();
-					String v = item.getString();
-					uploader.parameters.put(name, v);
-				}
-				else
-				{
-					uploader.items.add(item);
-				}
+				String name = item.getFieldName();
+				String v = item.getString();
+				uploader.parameters.put(name, v);
+			}
+			else
+			{
+				uploader.items.add(item);
 			}
 		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
 		return uploader;
+	}
+
+	public Map<String, String> upload(String upDir) throws Exception
+	{
+		Map<String, String> files = new HashMap<String, String>();
+		for (FileItem item : this.items)
+		{
+			String srcName = item.getName();
+			String randomName = UUID.randomUUID().toString() + srcName.substring(srcName.lastIndexOf("."));
+			File savedFile = new File(upDir, randomName);
+			item.write(savedFile);
+			files.put(item.getFieldName(), randomName);
+		}
+		return files;
 	}
 }
