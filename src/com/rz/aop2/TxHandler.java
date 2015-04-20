a8e062f57dc995110f5d850dc913196678e97f6e
ruanzy <ruanzhiyong6496@163.com>
2015-04-20 13:50:03 +0800

    optimize

---
+++ src/com/rz/aop2/TxHandler.java
@@ -0,0 +1,31 @@
+package com.rz.aop2;
+
+import com.rz.aop2.AOPHandler;
+import com.rz.dao.Dao;
+
+public class TxHandler implements AOPHandler
+{
+	Dao dao = Dao.getInstance();
+
+	public void doBefore()
+	{
+		dao.begin();
+	}
+
+	public void doAfter()
+	{
+		dao.commit();
+	}
+
+	public void doException()
+	{
+		dao.rollback();
+	}
+
+	public void doFinally()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/rz/aop2/UserService.java
+++ src/com/rz/aop2/UserService.java
@@ -1,7 +1,11 @@
 package com.rz.aop2;
 
+import com.rz.dao.Dao;
+
 public class UserService
 {
+	Dao dao = Dao.getInstance();
+	
 	public void save(String name)
 	{
 		System.out.println("我是save()方法");
