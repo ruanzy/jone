b7535418710e557d74a8f3dc0c646d6f5b50782c
ruanzy <ruanzhiyong6496@163.com>
2015-05-25 10:37:55 +0800

    1.增加 badge-*  css 2.head.html优化

---
+++ WebRoot/js/head.js
@@ -0,0 +1,88 @@
+var lang = 'zh';
+if ($.cookie('lang')) {
+	lang = $.cookie('lang');
+}
+var txt = (lang == "zh" ? "English" : "中文");
+$('#lang').html(txt);
+$('#info').click(function() {
+	var dialog = $.dialog({
+		//title:'个人资料',
+		width : 350,
+		//url:'view/income/list.html'
+		url : 'ftl/userinfo.html',
+		buttons : [ {
+			text : 'OK',
+			cls : 'btn-primary',
+			action : function(d) {
+
+			}
+		}, {
+			text : 'Close',
+			cls : 'btn-default',
+			action : function(d) {
+				dialog.close();
+			}
+
+		} ],
+		onShow : function() {
+			$('#sex').MultiRadio();
+		}
+	});
+});
+$('#set').click(function() {
+	var dialog = $.dialog({
+		title : '设置',
+		url : 'view/user/set.html',
+		buttons : [ {
+			text : 'OK',
+			cls : 'btn-primary',
+			action : function(d) {
+
+			}
+		}, {
+			text : 'Close',
+			cls : 'btn-default',
+			action : function(d) {
+				dialog.close();
+			}
+
+		} ],
+		onShow : function() {
+
+		}
+	});
+});
+$('#logout').click(function() {
+	document.location = 'logout';
+});
+$('#theme').SideDown({
+	width : 'auto',
+	url : 'theme.html'
+});
+$('#toggle').click(function() {
+	var sb = $('#sidebar');
+	if (sb.is(":visible")) {
+		$('#sidebar').hide();
+		$('#content').css('margin-left', 0);
+		$('#pill').css('left', 0);
+	} else {
+		$('#sidebar').show();
+		var sw = $('#sidebar').outerWidth();
+		$('#content').css('margin-left', sw);
+		$('#pill').css('left', sw);
+	}
+});
+$('#lang').click(function() {
+	var lang = 'zh';
+	if ($.cookie('lang')) {
+		lang = $.cookie('lang');
+	}
+	var txt = (lang == "zh" ? "中文" : "English");
+	lang = (lang == "zh" ? "en" : "zh");
+	$(this).html(txt);
+	$.cookie('lang', lang, {
+		path : '/',
+		expires : 10
+	});
+	document.location = '';
+});
