2c0cb838975802011dcaa4d2c5194a604bc17a59
ruanzy <ruanzhiyong6496@163.com>
2015-08-11 09:26:35 +0800

    task优化

--- src/com/rz/sql/Test.java
+++ src/com/rz/sql/Test.java
@@ -1,8 +1,6 @@
 package com.rz.sql;
 
-import java.util.Date;
 import com.alibaba.fastjson.JSON;
-import com.rz.schedule.Schedules2;
 
 public class Test
 {
@@ -12,32 +10,5 @@ public class Test
 		PageHelper.startPage(1, 3);
 		Pager p = PageHelper.pager(sql);
 		System.out.println(JSON.toJSON(p));
-
-		Schedules2.addTask("* * * * *", new Runnable()
-		{
-			public void run()
-			{
-				System.out.println(new Date());
-				System.out.println("A minute ticked away...");
-			}
-		});
-		Schedules2.addTask("*/2 * * * *", new Runnable()
-		{
-			public void run()
-			{
-				System.out.println(new Date());
-				System.out.println("Another minute ticked away...");
-			}
-		});
-		Schedules2.start();
-//		try
-//		{
-//			Thread.sleep(1000L * 60L * 5);
-//		}
-//		catch (InterruptedException e)
-//		{
-//			;
-//		}
-//		s.stop();
 	}
 }
