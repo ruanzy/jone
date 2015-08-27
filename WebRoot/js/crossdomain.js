93cb1d0e290d4005268d8b4152602a4d9f284d60
ruanzy <ruanzhiyong6496@163.com>
2015-08-27 16:37:20 +0800

    只是服务端

--- WebRoot/js/crossdomain.js
+++
@@ -1,5 +0,0 @@
-var url = 'http://localhost:8088/JOne/common/cookies';
-var scriptBlock = document.createElement("script");
-scriptBlock.setAttribute('src', url);
-scriptBlock.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(scriptBlock);
