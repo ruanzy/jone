a736b9e50408d343bfbf04209e8423b47e5c1b67
ruanzy <ruanzhiyong6496@163.com>
2016-09-05 10:54:33 +0800

    数据源从cfg.json读取

--- src/com/rz/dao/DBPool.java
+++ src/com/rz/dao/DBPool.java
@@ -5,13 +5,18 @@ import java.util.Map;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSourceFactory;
+import com.rz.util.Config;
 
-public class DBPool {
+public class DBPool
+{
 	static Map<String, DB> dbs = new HashMap<String, DB>();
-	static Map<String, Properties> dbProperties = new HashMap<String, Properties>();
-	static {
-		try {
-			for (Map.Entry<String, Properties> entry : dbProperties.entrySet()) {
+	static Map<String, Properties> dbProperties = loadDS();
+	static
+	{
+		try
+		{
+			for (Map.Entry<String, Properties> entry : dbProperties.entrySet())
+			{
 				String dsName = entry.getKey();
 				Properties p = entry.getValue();
 				dbProperties.put(dsName, p);
@@ -19,34 +24,65 @@ public class DBPool {
 				DB db = new DB(ds);
 				dbs.put(dsName, db);
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new DataAccessException("Create DataSource Exception!", e);
 		}
 	}
-	
-	public static Properties getDBProperties(String dsName){
+
+	public static Properties getDBProperties(String dsName)
+	{
 		return dbProperties.get(dsName);
 	}
 
-	public static DB getDB(String dsName) {
-		if (dbs.containsKey(dsName)) {
+	public static DB getDB(String dsName)
+	{
+		if (dbs.containsKey(dsName))
+		{
 			return dbs.get(dsName);
 		}
 		return null;
 	}
-	
-	public static DB createDB(String engine, String dsName, Properties prop) {
-		if (!dbs.containsKey(engine + '_' + dsName)) {
-			try {
+
+	public static DB createDB(String engine, String dsName, Properties prop)
+	{
+		if (!dbs.containsKey(engine + '_' + dsName))
+		{
+			try
+			{
 				dbProperties.put(engine + '_' + dsName, prop);
 				DataSource ds = BasicDataSourceFactory.createDataSource(prop);
 				DB db = new DB(ds);
 				dbs.put(dsName, db);
 				return db;
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				e.printStackTrace();
 			}
 		}
 		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static Map<String, Properties> loadDS()
+	{
+		Map<String, Properties> dbProperties = new HashMap<String, Properties>();
+		Map<String, Object> datasource = (Map<String, Object>) Config.getValue("datasource");
+		for (Map.Entry<String, Object> entry : datasource.entrySet())
+		{
+			String dsName = entry.getKey();
+			Map<String, Object> v = (Map<String, Object>) entry.getValue();
+			Properties p = new Properties();
+			p.putAll(v);
+			dbProperties.put(dsName, p);
+		}
+		return dbProperties;
+	}
+
+	public static void main(String[] args)
+	{
+
 	}
 }
