2f542cfe48e0e624e1b29d72867a23d2b65702bb
ruanzy <ruanzhiyong6496@163.com>
2016-10-26 17:39:18 +0800

    R的优化

--- src/com/rz/common/R.java
+++ src/com/rz/common/R.java
@@ -5,4 +5,19 @@ import java.util.HashMap;
 public class R extends HashMap<String, Object>
 {
 	private static final long serialVersionUID = 4112578634029874840L;
+
+	public String getString(String columnName)
+	{
+		return String.valueOf(this.get(columnName));
+	}
+
+	public Integer getInt(String columnName)
+	{
+		return Integer.valueOf(this.get(columnName).toString());
+	}
+
+	public Long getLong(String columnName)
+	{
+		return Long.valueOf(this.get(columnName).toString());
+	}
 }
