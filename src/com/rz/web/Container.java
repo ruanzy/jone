d0ffae4784052e3fa0bf023dbce6c66326fe1224
ruanzy <ruanzhiyong6496@163.com>
2016-10-28 18:12:28 +0800

    JOne.jar增加default.properties默认配置文件

--- src/com/rz/web/Container.java
+++ src/com/rz/web/Container.java
@@ -1,126 +1,185 @@
 package com.rz.web;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.rz.util.Config;
 import com.rz.util.Scaner;
 
 public class Container
 {
 	private static Logger log = LoggerFactory.getLogger(Container.class);
+	private static Properties cfg = new Properties();
 	private static Map<String, Object> actions = new HashMap<String, Object>();
 	private static Map<String, Interceptor> interceptors = new HashMap<String, Interceptor>();
 	private static List<Plugin> plugins = new ArrayList<Plugin>();
 
+	static
+	{
+		InputStream is = null;
+		try
+		{
+			String fileName = "defsult.properties";
+			is = Thread.currentThread().getContextClassLoader().getResourceAsStream("com/rz/web/" + fileName);
+			if (is != null)
+			{
+				log.debug("Loading " + fileName);
+				cfg.load(is);
+			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			try
+			{
+				if (is != null)
+				{
+					is.close();
+				}
+			}
+			catch (IOException e)
+			{
+
+			}
+		}
+	}
+
 	public static void init()
 	{
+		loadCfg();
 		loadActions();
 		loadInterceptors();
 		initPlugins();
 	}
 
-	private static void loadActions()
+	private static void loadCfg()
 	{
-		Object pck = Config.get("actionpck");
-		if (null != pck)
+		Properties p = new Properties();
+		InputStream is = null;
+		try
+		{
+			String fileName = "jone.properties";
+			is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+			if (is != null)
+			{
+				log.debug("Loading " + fileName);
+				p.load(is);
+				cfg.putAll(p);
+			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally
 		{
 			try
 			{
-				Set<Class<?>> _actions = Scaner.scan(pck.toString());
-				if (_actions.size() > 0)
+				if (is != null)
 				{
-					log.debug("Loading actions in " + pck);
-					for (Class<?> cls : _actions)
-					{
-						String name = cls.getName();
-						actions.put(name, cls.newInstance());
-						log.debug("Loading action " + name);
-					}
-					log.debug("Actions loaded!");
+					is.close();
 				}
 			}
-			catch (Exception e)
+			catch (IOException e)
 			{
-				throw new RuntimeException("Actions load error!", e);
+
 			}
+		}
+	}
+
+	private static void loadActions()
+	{
+		String pck = cfg.getProperty("action.package");
+		try
+		{
+			Set<Class<?>> _actions = Scaner.scan(pck);
+			if (_actions.size() > 0)
+			{
+				log.debug("Loading actions in " + pck);
+				for (Class<?> cls : _actions)
+				{
+					String name = cls.getName();
+					actions.put(name, cls.newInstance());
+					log.debug("Loading action " + name);
+				}
+				log.debug("Actions loaded!");
+			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("Actions load error!", e);
 		}
 	}
 
 	private static void loadInterceptors()
 	{
-		Object pck = Config.get("interceptorpck");
-		if (null != pck)
+		String pck = cfg.getProperty("interceptor.package");
+		try
 		{
-			try
+			Set<Class<?>> _interceptors = Scaner.scan(pck);
+			if (_interceptors.size() > 0)
 			{
-				Set<Class<?>> _interceptors = Scaner.scan(pck.toString());
-				if (_interceptors.size() > 0)
+				log.debug("Loading interceptors in " + pck + " package");
+				for (Class<?> cls : _interceptors)
 				{
-					log.debug("Loading interceptors in " + pck + " package");
-					for (Class<?> cls : _interceptors)
+					String name = cls.getName();
+					Expression expression = cls.getAnnotation(Expression.class);
+					if (expression != null)
 					{
-						String name = cls.getName();
-						Expression expression = cls.getAnnotation(Expression.class);
-						if (expression != null)
-						{
-							String exp = expression.value();
-							interceptors.put(exp, (Interceptor) cls.newInstance());
-						}
-						log.debug("Loading interceptor " + name);
+						String exp = expression.value();
+						interceptors.put(exp, (Interceptor) cls.newInstance());
 					}
-					log.debug("Interceptors loaded!");
+					log.debug("Loading interceptor " + name);
 				}
+				log.debug("Interceptors loaded!");
 			}
-			catch (Exception e)
-			{
-				throw new RuntimeException("Interceptors load error!", e);
-			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("Interceptors load error!", e);
 		}
 	}
 
 	private static void initPlugins()
 	{
-		Object pck = Config.get("pluginpck");
-		if (null != pck)
+		String pck = cfg.getProperty("plugin.package");
+		try
 		{
-			try
+			Set<Class<?>> _plugins = Scaner.scan(pck);
+			if (_plugins.size() > 0)
 			{
-				Set<Class<?>> _plugins = Scaner.scan(pck.toString());
-				if (_plugins.size() > 0)
+				log.debug("Loading plugins in " + pck);
+				for (Class<?> cls : _plugins)
 				{
-					log.debug("Loading plugins in " + pck);
-					for (Class<?> cls : _plugins)
-					{
-						plugins.add((Plugin) cls.newInstance());
-					}
-					for (Plugin plugin : plugins)
-					{
-						String name = plugin.getClass().getName();
-						log.debug("initing plugin " + name);
-						plugin.start();
-					}
+					plugins.add((Plugin) cls.newInstance());
+				}
+				for (Plugin plugin : plugins)
+				{
+					String name = plugin.getClass().getName();
+					log.debug("initing plugin " + name);
+					plugin.start();
 				}
 			}
-			catch (Exception e)
-			{
-				throw new RuntimeException("Plugins load error!", e);
-			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("Plugins load error!", e);
 		}
 	}
 
 	public static Object findAction(String actionName)
 	{
-		Object pck = Config.get("actionpck");
-		if (null != pck)
-		{
-			return actions.get(pck + "." + actionName);
-		}
-		return null;
+		String pck = cfg.getProperty("action.package");
+		return actions.get(pck + "." + actionName);
 	}
 
 	public static List<Interceptor> findInterceptor(String action, String method)
