176b1bbcafbaf5101f8a4b3ad44ba9e4377415f7
ruanzy <ruanzhiyong6496@163.com>
2016-12-05 10:15:35 +0800

    国际化支持

--- WebRoot/js/lib/util.js
+++ WebRoot/js/lib/util.js
@@ -1,6 +1,14 @@
 define(['template'], function(template){
 //var template = require('./template');
 localStorage.setItem('lang', 'zh');
+$.i18n.properties({
+	name : 'i18n',
+	path : 'i18n/',
+	mode : 'map',
+	language : localStorage.getItem('lang'),
+	cache : false,
+	encoding : 'UTF-8'
+});
 template.helper('i18n', function(key, params) {
 	return $.i18n.prop(key);
 });
@@ -42,32 +50,7 @@ var util = {
 		}else{
 			$(container).html(tpl);
 		}
-	},
-	log : function(msg) {
-		var now = new Date();
-		var time = now.Format('yyyy-MM-dd HH:mm:ss.S');
-		console.log(time + ' ' + msg);
 	}
-};
-//module.exports = obj;
-Date.prototype.Format = function(fmt) {
-	var o = {
-		"M+" : this.getMonth() + 1,
-		"d+" : this.getDate(),
-		"H+" : this.getHours(),
-		"m+" : this.getMinutes(),
-		"s+" : this.getSeconds(),
-		"q+" : Math.floor((this.getMonth() + 3) / 3),
-		"S" : this.getMilliseconds()
-	};
-	if (/(y+)/.test(fmt))
-		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
-				.substr(4 - RegExp.$1.length));
-	for ( var k in o)
-		if (new RegExp("(" + k + ")").test(fmt))
-			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
-					: (("00" + o[k]).substr(("" + o[k]).length)));
-	return fmt;
 };
 return util;
 });
--- WebRoot/js/main.js
+++ WebRoot/js/main.js
@@ -2,22 +2,20 @@ require.config({
 	paths : {
 		jquery : "lib/jquery-1.11.0.min",
 		bootstrap : "lib/bootstrap/js/bootstrap.min",
+		i18n : "lib/jquery.i18n.properties.min",
 		ztree : "lib/ztree/jquery.ztree.all.min",
 		template : "lib/template",
-		util : "lib/util",
 		dialog : "lib/dialog/dialog",
 		grid : "lib/grid/grid",
 		tabs : "lib/tabs/tabs",
-		highlight : "lib/highlight/highlight.pack"
+		highlight : "lib/highlight/highlight.pack",
+		util : "lib/util",
 	},
 	shim : {
 		"bootstrap" : [ "jquery", "css!lib/bootstrap/css/bootstrap.min", "css!lib/bootstrap/fonts/font-awesome.min"],
 		"ztree" : [ "css!lib/ztree/zTreeStyle/zTreeStyle"],
 		"highlight" : [ "css!lib/highlight/solarized-light"],
-		"util" : {
-			desp: [ "template"],
-			exports:'util'
-		},
+		"util" : [ "i18n", "template" ],
 		"dialog" : [ "util", "css!lib/dialog/dialog"],
 		"grid" : [ "css!lib/grid/grid"],
 		"tabs" : [ "util", "css!lib/tabs/tabs" ],
