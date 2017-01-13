2eef807cf9cb228fabc11452c5b8cdfba616a26f
ruanzy <ruanzhiyong6496@163.com>
2017-01-13 10:35:21 +0800

    优化

---
+++ src/com/rz/web/Uploader.java
@@ -0,0 +1,97 @@
+package com.rz.web;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import com.rz.web.WebUtil;
+
+public class Uploader
+{
+	static ServletFileUpload upload;
+	Map<String, String> parameters;
+	List<FileItem> items;
+
+	public Map<String, String> getParameters()
+	{
+		return parameters;
+	}
+
+	public List<FileItem> getItems()
+	{
+		return items;
+	}
+
+	private Uploader()
+	{
+		parameters = new HashMap<String, String>();
+		items = new ArrayList<FileItem>();
+	}
+
+	public static boolean isMultipart()
+	{
+		return ServletFileUpload.isMultipartContent(WebUtil.Request.get());
+	}
+
+	public static void init(long maxSize)
+	{
+		upload = new ServletFileUpload(new DiskFileItemFactory());
+		upload.setHeaderEncoding("utf-8");
+		if (maxSize > 0)
+		{
+			upload.setSizeMax(maxSize);
+		}
+	}
+
+	public static Uploader parse()
+	{
+		Uploader uploader = new Uploader();
+		try
+		{
+			List<FileItem> list = upload.parseRequest(WebUtil.Request.get());
+			for (FileItem item : list)
+			{
+				if (item.isFormField())
+				{
+					String name = item.getFieldName();
+					String v = item.getString();
+					uploader.parameters.put(name, v);
+				}
+				else
+				{
+					uploader.items.add(item);
+				}
+			}
+		}
+		catch (FileUploadException e)
+		{
+			e.printStackTrace();
+		}
+		return uploader;
+	}
+
+	public Map<String, String> upload(String upDir)
+	{
+		Map<String, String> files = new HashMap<String, String>();
+		for (FileItem item : this.items)
+		{
+			String srcName = item.getName();
+			File savedFile = new File(upDir, srcName);
+			try
+			{
+				item.write(savedFile);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			files.put(item.getFieldName(), savedFile.getPath());
+		}
+		return files;
+	}
+}
