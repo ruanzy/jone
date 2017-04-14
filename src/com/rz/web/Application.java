150fc1a8b6be2c8ff729848945cabf8b50b455bc
ruanzy <ruanzhiyong6496@163.com>
2017-04-14 17:59:21 +0800

    优化

--- src/com/rz/web/Application.java
+++ src/com/rz/web/Application.java
@@ -5,21 +5,22 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JOneServer
+public class Application
 {
-	static final Logger logger = LoggerFactory.getLogger(JOneServer.class);
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-	public static void start(String webRoot, int port) throws Exception
+	public static void run(String webRoot, int port) throws Exception
 	{
 		Server server = new Server(port);
 		WebAppContext webAppContext = new WebAppContext(webRoot, "");
 		webAppContext.setDescriptor(webRoot + "/WEB-INF/web.xml");
 		webAppContext.setResourceBase(webRoot);
+		webAppContext.setInitParameter("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false");
 		webAppContext.setClassLoader(Thread.currentThread().getContextClassLoader());
 		webAppContext.setConfigurationDiscovered(true);
 		webAppContext.setParentLoaderPriority(true);
 		server.setHandler(webAppContext);
 		server.start();
-		logger.info("JOneServer is started in port {}", port);
+		logger.info("Application is started in port {}", port);
 	}
 }
