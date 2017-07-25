763f93b833345a2d08378c572064cb723356e369
ruanzy <ruanzhiyong6496@163.com>
2017-07-25 14:47:34 +0800

    优化

--- src/com/rz/web/Launcher.java
+++ src/com/rz/web/Launcher.java
@@ -9,22 +9,33 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Launcher
 {
-	public static void main(String[] args) throws Exception
+	public static void start(int port)
 	{
-		int port = Integer.parseInt(args[0]);
 		Server server = new Server(port);
-		ProtectionDomain domain = Launcher.class.getProtectionDomain();
-		URL warLocation = domain.getCodeSource().getLocation();
-		String classpath = new File(warLocation.getFile()).getParentFile().getAbsolutePath();
-		WebAppContext webapp = new WebAppContext();
-		webapp.setContextPath("/");
-		webapp.setDescriptor(warLocation.toExternalForm() + "/WEB-INF/web.xml");
-		webapp.setServer(server);
-		webapp.setWar(warLocation.toExternalForm());
-		webapp.setTempDirectory(new File("./"));
-		webapp.setExtraClasspath(classpath);
-		server.setHandler(webapp);
-		server.start();
-		System.out.println("server is started in port " + port);
+		try {
+			server.setStopAtShutdown(true);
+            ProtectionDomain domain = Launcher.class.getProtectionDomain();
+            URL warLocation = domain.getCodeSource().getLocation();
+            String classpath = new File(warLocation.getFile()).getParentFile().getAbsolutePath();
+            WebAppContext webapp = new WebAppContext();
+            webapp.setContextPath("/");
+            webapp.setDescriptor(warLocation.toExternalForm() + "/WEB-INF/web.xml");
+            webapp.setServer(server);
+            webapp.setWar(warLocation.toExternalForm());
+            webapp.setTempDirectory(new File("./"));
+            webapp.setExtraClasspath(classpath);
+            server.setHandler(webapp);
+            server.start();
+            server.join();
+            System.out.println("server is started in port " + port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 	}
 }
