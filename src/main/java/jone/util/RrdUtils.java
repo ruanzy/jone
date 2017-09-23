e6c80b31d6abc26a4e11d25fdfcfb09d65960b80
ruanzy <ruanzhiyong6496@163.com>
2017-09-23 12:51:37 +0800

    优化

--- src/main/java/jone/util/RrdUtils.java
+++ src/main/java/jone/util/RrdUtils.java
@@ -6,6 +6,10 @@ import static org.rrd4j.DsType.GAUGE;
 import java.io.File;
 import java.io.IOException;
 
+import jone.R;
+
+import org.rrd4j.ConsolFun;
+import org.rrd4j.core.Archive;
 import org.rrd4j.core.FetchData;
 import org.rrd4j.core.FetchRequest;
 import org.rrd4j.core.RrdDb;
@@ -13,51 +17,41 @@ import org.rrd4j.core.RrdDef;
 import org.rrd4j.core.Sample;
 import org.rrd4j.core.Util;
 
-public class RrdUtils
-{
-	static final String RRDDIR = Cfg.getString("rrdDir");
+public class RrdUtils {
+	static final String RRDDIR = "e:/myrrd";
 	static final long START = Util.getTimestamp();
 	public static final int STEP = 5;
 	static final int YEAR = 60 * 60 * 24 * 365;
 	static final int DAY = 60 * 60 * 24;
 	static final int HOUR = 60 * 60;
+	static final int MINUTE = 60;
 
-	public static void createRrd(String fileName, String ds)
-	{
+	public static void createRrd(String fileName, String ds) {
 		String rrdPath = new File(RRDDIR, fileName).getPath();
 		RrdDef rrdDef = new RrdDef(rrdPath, START - 1, STEP);
 		rrdDef.addDatasource(ds, GAUGE, 2 * STEP, 0, Double.NaN);
-		rrdDef.addArchive(AVERAGE, 0.5, 1, YEAR/STEP);
-		rrdDef.addArchive(AVERAGE, 0.5, DAY/STEP, YEAR/DAY);
-		rrdDef.addArchive(AVERAGE, 0.5, HOUR/STEP, YEAR/HOUR);
+		rrdDef.addArchive(AVERAGE, 0.5, 1, YEAR / STEP);
+		rrdDef.addArchive(AVERAGE, 0.5, DAY / STEP, YEAR / DAY);
+		rrdDef.addArchive(AVERAGE, 0.5, HOUR / STEP, YEAR / HOUR);
+		rrdDef.addArchive(AVERAGE, 0.5, MINUTE / STEP, YEAR / MINUTE);
 		RrdDb rrdDb = null;
-		try
-		{
+		try {
 			rrdDb = new RrdDb(rrdDef);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally
-		{
+		} finally {
 
-			try
-			{
+			try {
 				rrdDb.close();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public static void writeRrd(String fileName, String ds, double v)
-	{
+	public static void writeRrd(String fileName, String ds, double v) {
 		RrdDb rrdDb = null;
-		try
-		{
+		try {
 			String rrdPath = new File(RRDDIR, fileName).getPath();
 			rrdDb = new RrdDb(rrdPath);
 			Sample sample = rrdDb.createSample();
@@ -65,27 +59,45 @@ public class RrdUtils
 			sample.setTime(time);
 			sample.setValue(ds, v);
 			sample.update();
-		}
-		catch (Exception e)
-		{
+			System.out.println(time + "====" + v);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static double[] readRrd(String fileName, String ds, long start, long end)
-	{
+	public static double[] readRrd(String fileName, String ds, long start, long end) {
 		RrdDb rrdDb = null;
-		try
-		{
+		try {
 			String rrdPath = new File(RRDDIR, fileName).getPath();
 			rrdDb = new RrdDb(rrdPath);
 			FetchRequest request = rrdDb.createFetchRequest(AVERAGE, start, end);
 			System.out.println(request.dump());
 			FetchData fetchData = request.fetchData();
 			return fetchData.getValues(ds);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch (Exception e)
-		{
+		return null;
+	}
+
+	public static R readLastValues(String fileName, String ds, int size, int steps) {
+		R r = new R();
+		RrdDb rrdDb = null;
+		try {
+			long resolution = steps * RrdUtils.STEP;
+			String rrdPath = new File(RRDDIR, fileName).getPath();
+			rrdDb = new RrdDb(rrdPath);
+			Archive archive = rrdDb.getArchive(ConsolFun.AVERAGE, steps);
+			long end = archive.getEndTime();
+			long start = end - (size - 1) * resolution;
+			FetchRequest request = rrdDb.createFetchRequest(AVERAGE, start, end, resolution);
+			FetchData fetchData = request.fetchData();
+			double[] values = fetchData.getValues(ds);
+			long[] timestamps = fetchData.getTimestamps();
+			r.put("timestamps", timestamps);
+			r.put("values", values);
+			return r;
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
