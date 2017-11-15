52c4f34d45a890281c95b1a1868f8e59e55d5aba
ruanzy <ruanzhiyong6496@163.com>
2017-11-15 15:01:31 +0800

    优化

--- src/main/java/jone/web/JarLauncher.java
+++ src/main/java/jone/web/JarLauncher.java
@@ -3,39 +3,34 @@ package jone.web;
 import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-
-import jone.util.Cfg;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JarLauncher
 {
-	public static void start()
+	public static void main(String[] args)
 	{
-		int port = Cfg.getInt("port", 8080);
-		String contextPath = Cfg.getString("context", "");
+		int port = 8080;
+		if(args != null){
+			try
+			{
+				port = Integer.valueOf(args[0].toString());
+			}
+			catch (Exception e)
+			{
+				throw new RuntimeException("The server port must be an integer of 0-65535");
+			}
+		}
 		Server server = new Server(port);
 		server.setStopAtShutdown(true);
 		ProtectionDomain protectionDomain = JarLauncher.class.getProtectionDomain();
 		URL location = protectionDomain.getCodeSource().getLocation();
 		String warFile = location.toExternalForm();
-		WebAppContext context = new WebAppContext(warFile, contextPath);
+		WebAppContext context = new WebAppContext(warFile, "");
 		try
 		{
-			context.setClassLoader(Thread.currentThread().getContextClassLoader());
 			context.setServer(server);
-			ServletHandler handler = new ServletHandler();
-			FilterHolder cors = handler.addFilterWithMapping(CorsFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-			context.addFilter(cors, "/*", EnumSet.of(DispatcherType.REQUEST));
-			FilterHolder jone = handler.addFilterWithMapping(JOne.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-			jone.setInitParameter("basepackage", Cfg.getString("basepackage"));
-			context.addFilter(jone, "/*", EnumSet.of(DispatcherType.REQUEST));
 	        String currentDir = new File(location.getPath()).getParent();
 	        File workDir = new File(currentDir, "work");
 	        context.setTempDirectory(workDir);
--- src/main/java/jone/web/Launcher.java
+++ src/main/java/jone/web/Launcher.java
@@ -1,38 +1,20 @@
 package jone.web;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-
-import jone.util.Cfg;
-
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Launcher
 {
-	public static void start()
+	public static void start(int port, String context)
 	{
-		int port = Cfg.getInt("port", 8080);
-		String contextPath = Cfg.getString("context", "");
 		String WebRoot = "src/main/webapp";
 		Server server = new Server(port);
 		server.setStopAtShutdown(true);
-		WebAppContext wc = new WebAppContext(WebRoot, contextPath);
+		WebAppContext wc = new WebAppContext(WebRoot, context);
 		wc.setDescriptor(WebRoot + "/WEB-INF/web.xml");
 		wc.setResourceBase(WebRoot);
 		wc.setInitParameter("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false");
-		wc.setClassLoader(Thread.currentThread().getContextClassLoader());
 		wc.setConfigurationDiscovered(true);
-		wc.setParentLoaderPriority(true);
-		ServletHandler handler = new ServletHandler();
-		FilterHolder cors = handler.addFilterWithMapping(CorsFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-		wc.addFilter(cors, "/*", EnumSet.of(DispatcherType.REQUEST));
-		FilterHolder jone = handler.addFilterWithMapping(JOne.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-		jone.setInitParameter("basepackage", Cfg.getString("basepackage"));
-        wc.addFilter(jone, "/*", EnumSet.of(DispatcherType.REQUEST));
 		server.setHandler(wc);
 		try
 		{
