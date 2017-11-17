e5ed55771d3e07b4f92ad47451eb859062a5e635
ruanzy <ruanzhiyong6496@163.com>
2017-11-17 14:08:45 +0800

    优化

--- src/main/java/jone/web/Banner.java
+++ src/main/java/jone/web/Banner.java
@@ -9,13 +9,13 @@ public class Banner
 	public static void print()
 	{
 		ClassLoader classLoader = JOne.class.getClassLoader();
-		InputStream is = classLoader.getResourceAsStream("/jone/logo.txt");
+		InputStream is = classLoader.getResourceAsStream("jone/logo.txt");
 		try
 		{
 			String logo = IOUtils.toString(is);
 			String version = JOne.class.getPackage().getImplementationVersion();
 			logo = logo.replaceFirst("\\$\\{version\\}", version);
-			System.out.println("\n" + logo);
+			System.out.println("\r" + logo);
 		}
 		catch (Exception e)
 		{
